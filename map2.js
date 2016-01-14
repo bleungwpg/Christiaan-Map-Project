0774d74c59a171ece2da14a9de759a47fa162e96
aisbleung <bleung@ais.edu.hk>
2016-01-14 10:40:49 +0800

    v1b

--- map2.js
+++ map2.js
@@ -1,53 +1,22 @@
-var img;
 function preload() {
   img = loadImage("images/fourcolor.png");
 }
 function setup() {
-  createCanvas(1000,500);
-//  image(img, 0, 0);
-//  img.loadPixels();
-  i = 0
+  createCanvas(150,150);
+  image(img, 0, 0);
+  loadPixels();
+
+
+
+  d = pixelDensity();
+  row = 1;
+  for (i = 0, j = 0; j < 150*d+20; i=i+4, j++)
+  {
+	print(j+": "+pixels[i]+"  "+pixels[i+1]+"  "+pixels[i+2]+"  "+pixels[i+3]+"  ");
+  }
+  
 }
 function draw()
 {
-  background(0,0,0);
-  image(img, 0, 0);
-  loadPixels();
-  fill(255,0,0);
-
-  row = 2;
-  x = 0;
-  y = 0;
-
-/*
-  image(img, 0, 0);
-  c = get(10,0);
-  fill(255,0,0);
-  rect(200,10,20,20);
-  */
-//  text(get(10,0),200,10);
-//  text(pixelDensity(),200,10);
-
-
-
-  
-//  for (i = 25000; i < 25020; i++)
-  for (i = row*580; i < row*630; i++)
-  {
-//    if (pixels[i] != 255 && pixels[i] != 0)
-//    {
-		text(i,x+200,(y+1)*25);
-		text(pixels[i],x+75+200,(y+1)*25);
-		y++;   
-		if (y == 12)
-		{
-			y = 0;
-			x = x + 150;
-		}
-
-//    }
-
-  }
-  
 
 }
